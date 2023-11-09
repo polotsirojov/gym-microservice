ad9d0a4f4451e310bb56fe8517fd614e64cc2932
Po'lot Sirojov <polot_sirojov@epam.com>
2023-11-09 11:17:39 +0500

    jms config

--- gym/src/main/java/com/polot/gym/GymApplication.java
+++ gym/src/main/java/com/polot/gym/GymApplication.java
@@ -6,11 +6,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableJms
 public class GymApplication {
 
 	public static void main(String[] args) {
